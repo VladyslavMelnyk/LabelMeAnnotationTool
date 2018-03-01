60118de891bfad9afe86b214116c4e9344ca1f39
VladyslavMelnyk <as-vlad@ukr.net>
2018-03-01 17:10:02 +0200

    fix to start container

--- start_labels.sh
+++ start_labels.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker rm -f labels
-docker run --name labels -v /home/lv_user/LabelMeAnnotationTool/:/var/www/html labelme
+docker run --name labels -v /home/lv_user/LabelMeAnnotationTool/:/var/www/html labelme \
+&& docker exec -d labels -c "cd /var/www/html && make" &
 
