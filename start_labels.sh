273c4dd032b1fd162265eecacff0a13b1036c3ce
VladyslavMelnyk <as-vlad@ukr.net>
2018-03-13 16:34:28 +0200

    fixed start script

--- start_labels.sh
+++ start_labels.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker rm -f labels
-docker run --name labels -v /home/lv_user/LabelMeAnnotationTool/:/var/www/html labelme \
-&& docker exec -d labels -c "cd /var/www/html && make" &
+docker run -d --name labels -v $PWD:/var/www/html labelme
+docker exec -d labels bash -c "cd /var/www/html && make" 
+docker start labels
 
