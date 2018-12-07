584d9c460a5ea892455735df69ea91864d52ecbd
Vladyslav_Melnyk <twinchelseaman@gmail.com>
2018-12-07 15:45:31 +0100

    fix

--- start_labels.sh
+++ start_labels.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker rm -f labels
-sudo rm -drf $PWD/annotationCache/TmpAnnotations/*
-sudo chown -R www-data $PWD && sudo chmod -R 774 $PWD && sudo chmod -R 700 $PWD/annotationCache/TmpAnnotations
+sudo chown -R www-data $PWD && sudo chmod -R 774 $PWD && sudo chmod -R 774 $PWD/annotationCache/TmpAnnotations
 docker run -d -p 80:80  --name labels -v $PWD/:/var/www/html labelme
 docker exec -d labels bash -c "cd /var/www/html && make"
+rm $PWD/annotationCache/DirLists/labelme.txt
 cd $PWD/annotationTools/sh/ && ./populate_dirlist.sh
 docker start labels
